f4acc0a26e3f050a5b79c9488e684b51bd85fd5b
leitegs <leitedasilvag@gmail.com>
2020-12-22 00:11:27 -0300

    use variable global

--- src/config/mail.js
+++ src/config/mail.js
@@ -2,7 +2,7 @@ export default {
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
   auth: {
-    user: process.env.USER_MAIL,
-    pass: process.env.USER_PASS
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 }
